b0d164b1399cf86ff41ab02c090837232127dcec
sahlhafidzirkhami <sahlhafidzirkhami@gmail.com>
2025-07-21 14:02:22 +0700

    feat(installation):migration to Barudak EVOS company

---
+++ app/Views/base/beranda/v_about_us.php
@@ -0,0 +1,14 @@
+<div class="container" style="padding-top: 120px; padding-bottom: 50px;">
+    <div class="row">
+        <div class="col-md-12">
+            <h2>Tentang Kami</h2>
+            <hr>
+            <p>
+                <?= SITE_NAME ?> adalah layanan undangan online. Yaitu undangan yang dikemas dalam bentuk web yang praktis dan mudah untuk digunakan maupun dibagikan. Selain itu kami juga menerima jasa pembuatan undangan cetak maupun Video.
+            </p>
+            <p>
+                Informasi lebih lanjut tentang perusahaan atau tim Anda bisa diletakkan di sini.
+            </p>
+        </div>
+    </div>
+</div>
